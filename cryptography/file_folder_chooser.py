57dbccfc092c69f8717db7e74f5893efc5d649d2
randalthor17 <83530912+randalthor17@users.noreply.github.com>
2021-05-27 05:50:19 +0600

    Update file_folder_chooser.py

--- cryptography/file_folder_chooser.py
+++ cryptography/file_folder_chooser.py
@@ -2,11 +2,24 @@ from tkinter import Tk
 from tkinter.filedialog import askopenfilename, askdirectory
 
 def file_chooser():
-    Tk().withdraw()
+    main_win = Tk()
+    main_win.withdraw()
+    main_win.overrideredirect(True)
+    main_win.geometry('0x0+0+0')
+    main_win.deiconify()
+    main_win.lift()
+    main_win.focus_force()
     filename = askopenfilename()
+    main_win.destroy()
     return filename
 
 def folder_chooser():
-    Tk().withdraw()
+    main_win = Tk()
+    main_win.withdraw()
+    main_win.overrideredirect(True)
+    main_win.geometry('0x0+0+0')
+    main_win.deiconify()
+    main_win.lift()
+    main_win.focus_force()
     foldername = askdirectory()
     return foldername
